a30088a13b28d43d00748b474bc5b6c1f19e1f10
Parasmita <parasmita@railsfactory.org>
2022-03-17 10:16:54 +0530

    rating slider added

--- ecom_app_ui/src/components/catalogue.js
+++ ecom_app_ui/src/components/catalogue.js
@@ -57,13 +57,29 @@ export default function Catalogue() {
     );
   };
 
-  const [value2, setValue2] = React.useState([20, 37]);
+  const [value2, setValue2] = React.useState([0, 15]);
+  const [value1, setValue1] = React.useState([0, 10]);
 
+  const handleChange1 = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 5 - minDistance);
+        setValue1([clamped, clamped + minDistance]);
+      } else {
+        const clamped = Math.max(newValue[1], minDistance);
+        setValue1([clamped - minDistance, clamped]);
+      }
+    } else {
+      setValue1(newValue);
+    }
+  };
   const handleChange2 = (event, newValue, activeThumb) => {
     if (!Array.isArray(newValue)) {
       return;
     }
-
     if (newValue[1] - newValue[0] < minDistance) {
       if (activeThumb === 0) {
         const clamped = Math.min(newValue[0], 100 - minDistance);
@@ -134,8 +150,8 @@ export default function Catalogue() {
                 </ListItemButton>
                 <Slider
                   getAriaLabel={() => "Minimum distance shift"}
-                  value={value2}
-                  onChange={handleChange2}
+                  value={value1}
+                  onChange={handleChange1}
                   valueLabelDisplay="auto"
                   getAriaValueText={valuetext}
                   disableSwap

166ed4c6613b874296259efba510a89e030bb290
Parasmita <parasmita@railsfactory.org>
2022-04-01 20:56:53 +0530

    product showing issue fixes

--- ecom_app_ui/src/components/catalogue.js
+++ ecom_app_ui/src/components/catalogue.js
@@ -1,235 +1,204 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchCatelogues } from "../redux/catelogue/catelogueAction";
-// import Navbar from "./navbar";
-// import Button from "@mui/material/Button";
-// import Card from "@mui/material/Card";
-// import CardActions from "@mui/material/CardActions";
-// import CardContent from "@mui/material/CardContent";
-// import CardMedia from "@mui/material/CardMedia";
-// import CssBaseline from "@mui/material/CssBaseline";
-// import Grid from "@mui/material/Grid";
-// import Box from "@mui/material/Box";
-// import Typography from "@mui/material/Typography";
-// import Container from "@mui/material/Container";
-// import { createTheme, ThemeProvider } from "@mui/material/styles";
-// import Rating from "@mui/material/Rating";
-// import List from "@mui/material/List";
-// import ListItem from "@mui/material/ListItem";
-// import ListItemButton from "@mui/material/ListItemButton";
-// import ListItemText from "@mui/material/ListItemText";
-// import Select from "@mui/material/Select";
-// import Checkbox from "@mui/material/Checkbox";
-// import OutlinedInput from "@mui/material/OutlinedInput";
-// import InputLabel from "@mui/material/InputLabel";
-// import MenuItem from "@mui/material/MenuItem";
-// import FormControl from "@mui/material/FormControl";
-// import Slider from "@mui/material/Slider";
+import Navbar from "./navbar";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Rating from "@mui/material/Rating";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Select from "@mui/material/Select";
+import Checkbox from "@mui/material/Checkbox";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Slider from "@mui/material/Slider";
 
 // const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const minDistance = 10;
+const minDistance = 10;
 
-// function valuetext(value2) {
-//   return `${value2}°C`;
-// }
-// const theme = createTheme();
-// const ITEM_HEIGHT = 28;
-// const ITEM_PADDING_TOP = 8;
-// const MenuProps = {
-//   PaperProps: {
-//     style: {
-//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-//       width: 350,
-//     },
-//   },
-// };
+function valuetext(value2) {
+  return `${value2}°C`;
+}
+const theme = createTheme();
+const ITEM_HEIGHT = 28;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 350,
+    },
+  },
+};
 
-// const names = ["Chanel", "Prada", "Gucci Jackie"];
+const names = ["Chanel", "Prada", "Gucci Jackie"];
 
 function Catalogue({ productData, fetchCatelogues }) {
-  // const [personName, setPersonName, value, setValue] = React.useState([]);
+  const [personName, setPersonName, value, setValue] = React.useState([]);
 
-  // const handleChange = (event) => {
-  //   const {
-  //     target: { value },
-  //   } = event;
-  //   setPersonName(
-  //     // On autofill we get a stringified value.
-  //     typeof value === "string" ? value.split(",") : value
-  //   );
-  // };
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
 
-  // const [value2, setValue2] = React.useState([0, 15]);
+  const [value2, setValue2] = React.useState([0, 15]);
 
-  // const handleChange2 = (event, newValue, activeThumb) => {
-  //   if (!Array.isArray(newValue)) {
-  //     return;
-  //   }
-  //   if (newValue[1] - newValue[0] < minDistance) {
-  //     if (activeThumb === 0) {
-  //       const clamped = Math.min(newValue[0], 100 - minDistance);
-  //       setValue2([clamped, clamped + minDistance]);
-  //     } else {
-  //       const clamped = Math.max(newValue[1], minDistance);
-  //       setValue2([clamped - minDistance, clamped]);
-  //     }
-  //   } else {
-  //     setValue2(newValue);
-  //   }
-  // };
+  const handleChange2 = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
+    if (newValue[1] - newValue[0] < minDistance) {
+      if (activeThumb === 0) {
+        const clamped = Math.min(newValue[0], 100 - minDistance);
+        setValue2([clamped, clamped + minDistance]);
+      } else {
+        const clamped = Math.max(newValue[1], minDistance);
+        setValue2([clamped - minDistance, clamped]);
+      }
+    } else {
+      setValue2(newValue);
+    }
+  };
   useEffect(() => {
     fetchCatelogues();
   }, []);
 
-  return productData && productData.all ? (
+  return (
     <div>
-      <h2>All Product List</h2>
-
-      {productData &&
-        productData.all &&
-        productData.all.map((product) => (
-          <table>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Price</th>
-              <th>Ratings</th>
-            </tr>
-          <tr>
-            <td> {product.name}</td>
-            <td>{product.description}</td>
-            <td>{product.price}</td>
-            <td>{product.ratings}</td>
-          </tr></table>
-        ))}
-    </div>
-  ) : productData && productData.error ? (
-    <h2>{productData.error}</h2>
-  ) : (
-    <div>
-      <div>
-        {productData &&
-          productData.all &&
-          productData.all.map((product) => <p> {product.name}</p>)}
-      </div>
-    </div>
+    <Navbar />
+    <Grid container direction={"row"} spacing={5}>
+      <Box
+        sx={{ width: "100%", maxWidth: 260, bgcolor: "background.paper" }}
+        mx={10}
+        my={10}
+      >
+        <nav aria-label="main mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Brand" />
+              </ListItemButton>
+              <FormControl sx={{ m: 1, width: 300 }}>
+                <InputLabel id="demo-multiple-checkbox-label">
+                  Brand
+                </InputLabel>
+                <Select
+                  labelId="demo-multiple-checkbox-label"
+                  id="demo-multiple-checkbox"
+                  multiple
+                  value={personName}
+                  onChange={handleChange}
+                  input={<OutlinedInput label="Brand" />}
+                  renderValue={(selected) => selected.join(", ")}
+                  MenuProps={MenuProps}
+                >
+                  {names.map((name) => (
+                    <MenuItem key={name} value={name}>
+                      <Checkbox checked={personName.indexOf(name) > -1} />
+                      <ListItemText primary={name} />
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Price" />
+              </ListItemButton>
+              <Slider
+                getAriaLabel={() => "Minimum distance shift"}
+                value={value2}
+                onChange={handleChange2}
+                valueLabelDisplay="auto"
+                getAriaValueText={valuetext}
+                disableSwap
+              />
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Rating" />
+              </ListItemButton>
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }}
+              />
+            </ListItem>
+          </List>
+        </nav>
+      </Box>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <main>
+          <Box
+            sx={{
+              bgcolor: "background.paper",
+            }}
+          ></Box>
+          <Container sx={{ py: 10, px: 0 }} maxWidth="lg">
+            <Grid container spacing={5}>
+              {productData.all.map((product) => (
+                <Grid item key={product} xs={12} sm={12} md={3}>
+                  <Card
+                    sx={{
+                      width: "100%",
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                    my={20}
+                  >
+                    <CardMedia
+                      component="img"
+                      image="https://source.unsplash.com/random"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {product.name}
+                      </Typography>
+                      <Typography>
+                        {product.description}
+                      </Typography>
+                      <Typography>RS.{product.price}</Typography>
+                      <Typography>
+                        rating
+                        <Rating name="read-only" value={product.rating} readOnly />
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Add To Cart</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+      </ThemeProvider>
+    </Grid>
+  </div>
   );
-  // <div>
-  //   <Navbar />
-  //   <Grid container direction={"row"} spacing={5}>
-  //     <Box
-  //       sx={{ width: "100%", maxWidth: 260, bgcolor: "background.paper" }}
-  //       mx={10}
-  //       my={10}
-  //     >
-  //       <nav aria-label="main mailbox folders">
-  //         <List>
-  //           <ListItem disablePadding>
-  //             <ListItemButton>
-  //               <ListItemText primary="Brand" />
-  //             </ListItemButton>
-  //             <FormControl sx={{ m: 1, width: 300 }}>
-  //               <InputLabel id="demo-multiple-checkbox-label">
-  //                 Brand
-  //               </InputLabel>
-  //               <Select
-  //                 labelId="demo-multiple-checkbox-label"
-  //                 id="demo-multiple-checkbox"
-  //                 multiple
-  //                 value={personName}
-  //                 onChange={handleChange}
-  //                 input={<OutlinedInput label="Brand" />}
-  //                 renderValue={(selected) => selected.join(", ")}
-  //                 MenuProps={MenuProps}
-  //               >
-  //                 {names.map((name) => (
-  //                   <MenuItem key={name} value={name}>
-  //                     <Checkbox checked={personName.indexOf(name) > -1} />
-  //                     <ListItemText primary={name} />
-  //                   </MenuItem>
-  //                 ))}
-  //               </Select>
-  //             </FormControl>
-  //           </ListItem>
-  //           <ListItem disablePadding>
-  //             <ListItemButton>
-  //               <ListItemText primary="Price" />
-  //             </ListItemButton>
-  //             <Slider
-  //               getAriaLabel={() => "Minimum distance shift"}
-  //               value={value2}
-  //               onChange={handleChange2}
-  //               valueLabelDisplay="auto"
-  //               getAriaValueText={valuetext}
-  //               disableSwap
-  //             />
-  //           </ListItem>
-  //           <ListItem disablePadding>
-  //             <ListItemButton>
-  //               <ListItemText primary="Rating" />
-  //             </ListItemButton>
-  //             <Rating
-  //               name="simple-controlled"
-  //               value={value}
-  //               onChange={(event, newValue) => {
-  //                 setValue(newValue);
-  //               }}
-  //             />
-  //           </ListItem>
-  //         </List>
-  //       </nav>
-  //     </Box>
-  //     <ThemeProvider theme={theme}>
-  //       <CssBaseline />
-  //       <main>
-  //         <Box
-  //           sx={{
-  //             bgcolor: "background.paper",
-  //           }}
-  //         ></Box>
-  //         <Container sx={{ py: 10, px: 0 }} maxWidth="lg">
-  //           <Grid container spacing={5}>
-  //             {cards.map((card) => (
-  //               <Grid item key={card} xs={12} sm={12} md={3}>
-  //                 <Card
-  //                   sx={{
-  //                     width: "100%",
-  //                     height: "100%",
-  //                     display: "flex",
-  //                     flexDirection: "column",
-  //                   }}
-  //                   my={20}
-  //                 >
-  //                   <CardMedia
-  //                     component="img"
-  //                     image="https://source.unsplash.com/random"
-  //                     alt="random"
-  //                   />
-  //                   <CardContent sx={{ flexGrow: 1 }}>
-  //                     <Typography gutterBottom variant="h5" component="h2">
-  //                       Heading
-  //                     </Typography>
-  //                     <Typography>
-  //                       This is a media card. You can use this section to
-  //                       describe the content.
-  //                     </Typography>
-  //                     <Typography>RS.1000</Typography>
-  //                     <Typography>
-  //                       <Rating name="read-only" value={4} readOnly />
-  //                     </Typography>
-  //                   </CardContent>
-  //                   <CardActions>
-  //                     <Button size="small">Add To Cart</Button>
-  //                   </CardActions>
-  //                 </Card>
-  //               </Grid>
-  //             ))}
-  //           </Grid>
-  //         </Container>
-  //       </main>
-  //     </ThemeProvider>
-  //   </Grid>
-  // </div>
 }
 
 const mapStateToProps = (state) => {

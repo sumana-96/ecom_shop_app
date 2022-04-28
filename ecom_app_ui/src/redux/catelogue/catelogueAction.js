59ca510ff2deaec2fb809c09f6765c88a9c2267b
railsfactory-sumana <sumana@railsfactory.org>
2022-04-28 10:52:03 +0530

    removed extra code

--- ecom_app_ui/src/redux/catelogue/catelogueAction.js
+++ ecom_app_ui/src/redux/catelogue/catelogueAction.js
@@ -1,31 +1,11 @@
-// import axios from 'axios'
 import { catelogueActionTypes } from './catelogueType';
 import axios from 'axios';
 
 
 export const fetchCatelogues = () => async (dispatch) => {
-  console.log('action api called')
-  // const res = await API.get('/v1/categories');
-  // const res = await axios.get('https://jsonplaceholder.typicode.com/users');
-  // const res =  axios.get('http://localhost:5000/list_products');
-  // console.log(res)
-  // if (res.status === 200) {
-  //   return dispatch({
-  //     type: catelogueActionTypes.FETCH_CATELOGUE_SUCCESS,
-  //     all: res.catelogues,
-  //     popular: res.popularCatelogues,
-  //   });
-  // } else {
-  //   return dispatch({
-  //     type: catelogueActionTypes.FETCH_CATELOGUE_FAILURE,
-  //     message: res.message,
-  //   });
-  //   console.log('err====', res.message);
-  // }
   axios.get('http://localhost:5000/list_products', 
     )
   .then(response => { 
-    // console.log(response)
     if (response.status === 200) {
         return dispatch({
           type: catelogueActionTypes.FETCH_CATELOGUE_SUCCESS,
